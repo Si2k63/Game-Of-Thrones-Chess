172cee62de97de6ba4c550b4f9aeb2390af85834
Simon Phillips <poc_is_good@hotmail.com>
2023-02-05 16:10:16 +0000

    Create empty board, add tailwind, remove unneeded boiler plate code from base vite project.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react'
+import './App.css'
+import Board from './components/Board'
+import Piece from './components/Piece/Piece'
+
+const pieces: (ReactElement|null)[][] = [
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+]
+
+function App() {
+
+  return (
+    <div className="App">
+      <Board pieces={pieces} />
+    </div>
+  )
+}
+
+export default App
