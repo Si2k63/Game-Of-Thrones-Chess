1dc48e479e57a6dc25a1314c27755cd2d0593795
Simon Phillips <poc_is_good@hotmail.com>
2023-02-08 20:06:20 +0000

    Filled out board

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Board from './components/Board'
 import Piece from './components/Piece/Piece'
 
 const pieces: (ReactElement|null)[][] = [
-  [<Piece colour='Black' type="Knight" />, null, null, null, null, null, null, null],
+  [<Piece colour='Black' type="Rook" />, <Piece colour='Black' type='Knight' />, <Piece colour='Black' type='Bishop' />, <Piece colour='Black' type='Queen' />, <Piece colour='Black' type='King' />, <Piece colour='Black' type='Bishop' />, <Piece colour='Black' type='Knight' />, <Piece colour='Black' type='Rook' />],
+  [<Piece colour='Black' type="Pawn" />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />, <Piece colour='Black' type='Pawn' />],
   [null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
+  [<Piece colour='White' type="Pawn" />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />, <Piece colour='White' type='Pawn' />],
+  [<Piece colour='White' type="Rook" />, <Piece colour='White' type='Knight' />, <Piece colour='White' type='Bishop' />, <Piece colour='White' type='Queen' />, <Piece colour='White' type='King' />, <Piece colour='White' type='Bishop' />, <Piece colour='White' type='Knight' />, <Piece colour='White' type='Rook' />],
 ]
 
 function App() {
