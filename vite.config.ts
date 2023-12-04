d198106051f91b634776220b2a73798c8fbd59c0
Simon Phillips <poc_is_good@hotmail.com>
2023-12-04 21:09:02 +0000

    Changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Game-Of-Thrones-Chess/',
   plugins: [react()],
   resolve: {
     alias: {
