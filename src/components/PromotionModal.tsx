a968448f0f1af74293fad0df50ada4866b605b70
Simon Phillips <poc_is_good@hotmail.com>
2025-12-31 16:51:31 +0000

    refactor: create hook for promotion modal

--- src/components/PromotionModal.tsx
+++ src/components/PromotionModal.tsx
@@ -1,49 +1,12 @@
-import Rook from "@/engine/pieces/Rook";
-import useApplicationContext from "@/hooks/useApplicationContext";
-import { useChessEngine } from "@/hooks/useChessEngine";
+import usePromotionModal from "@/hooks/usePromotionModal";
 import Piece from "./Piece";
-import Knight from "@/engine/pieces/Knight";
-import Bishop from "@/engine/pieces/Bishop";
-import Queen from "@/engine/pieces/Queen";
-import { TPiece } from "@/engine/Engine.types";
 
 const PromotionModal = () => {
-  const [state, setState] = useApplicationContext();
-  const {
-    result,
-    result: { canPromote, previous },
-  } = state;
 
-  const { onPromote } = useChessEngine();
-
-  const classes = [
-    "fixed inset-0 items-center justify-center bg-black bg-opacity-20 z-50",
-  ];
-
-  if (!canPromote) {
-    classes.push("hidden");
-  }
-
-  const onPieceSelect = (piece: TPiece) => {
-    onPromote(piece);
-    setState({
-      ...state,
-      result: {
-        ...result,
-        canPromote: false,
-      },
-    });
-  };
-
-  const pieces: TPiece[] = [
-    new Rook(previous, "Left"),
-    new Knight(previous, "Left"),
-    new Bishop(previous, "Left"),
-    new Queen(previous),
-  ];
+  const { onPieceSelect, className, pieces } = usePromotionModal();
 
   return (
-    <div className={classes.join(" ")}>
+    <div className={className}>
       <div className="bg-gray-900 rounded-sm max-w-md w-full p-6 space-y-4 absolute transform translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2">
         <h3 className="text-xl text-white font-semibold">
           Choose Your Warrior
@@ -58,7 +21,7 @@ const PromotionModal = () => {
                 {...piece}
                 key={key}
                 underAttack={false}
-                active={false}
+                active={true}
                 onClick={() => onPieceSelect(piece)}
               />
             ))}
---
+++ src/hooks/usePromotionModal.ts
@@ -0,0 +1,52 @@
+import Rook from "@/engine/pieces/Rook";
+import useApplicationContext from "@/hooks/useApplicationContext";
+import { useChessEngine } from "@/hooks/useChessEngine";
+import Knight from "@/engine/pieces/Knight";
+import Bishop from "@/engine/pieces/Bishop";
+import Queen from "@/engine/pieces/Queen";
+import { TPiece } from "@/engine/Engine.types";
+
+const usePromotionModal = () => {
+
+  const [state, setState] = useApplicationContext();
+  const {
+    result,
+    result: { canPromote, previous },
+  } = state;
+
+  const { onPromote } = useChessEngine();
+
+  const classes = [
+    "fixed inset-0 items-center justify-center bg-black bg-opacity-20 z-50",
+  ];
+
+  if (!canPromote) {
+    classes.push("hidden");
+  }
+
+  const onPieceSelect = (piece: TPiece) => {
+    onPromote(piece);
+    setState({
+      ...state,
+      result: {
+        ...result,
+        canPromote: false,
+      },
+    });
+  };
+
+  const pieces: TPiece[] = [
+    new Rook(previous, "Left"),
+    new Knight(previous, "Left"),
+    new Bishop(previous, "Left"),
+    new Queen(previous),
+  ];
+
+  return {
+    pieces,
+    onPieceSelect,
+    className: classes.join(" ")
+  }
+}
+
+export default usePromotionModal;
